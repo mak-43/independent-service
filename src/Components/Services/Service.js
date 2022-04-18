dca66d355db69f67632f8b3d9aabbf37262cd047
Mak-43 <letsmailmak@gmail.com>
2022-04-18 09:16:41 +0600

    cart design

---
+++ src/Components/Services/Service.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Service.css'
+
+const Service = ({service}) => {
+    const {name,id,img,price,description}=service
+    return (
+        <div className='service flex flex-col justify-center items-center  m-10'>
+            <div className='flex justify-center items-center'>
+                <img className='rounded-xl mt-2' src={img} alt="" />
+            </div>
+            <div className='flex flex-col text-left justify-center items-center p-5'>
+                <p className='text-xl font-semibold pb-3'>{name}</p>
+                <p>{description}</p>
+                <p className='text-xl font-bold'>{price}</p>
+                <button className='text-black bg-gray-400 font-bold py-2 px-5 my-2 submit rounded-xl hover:text-blue-700 '>Checkout</button>
+            </div>
+          
+        </div>
+    );
+};
+
+export default Service;
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from './Service';
+
 
 const Services = () => {
+
+    const [services,setServices]=useState([])
+    useEffect(()=>{
+        fetch('services.json')
+        .then(res=>res.json())
+        .then(data=>setServices(data))
+    },[])
+
     return (
         <div>
-            Services
-            <form  className='flex flex-col w-1/2 mx-auto '>
-                    <h1 className='text-3xl pb-5'>Please Login</h1>
-                    <input type="email" name="email" id="" placeholder='Email' required />
-                    <input  type="password" name="password" id="" placeholder='Password' required />
-                    <input className='text-black bg-gray-400 font-bold py-2 submit rounded-xl hover:text-blue-700 ' type="submit" value="Login" />
-                </form>
+            <h1 className='text-4xl font-semibold '>Legal Practices Area</h1>
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5'>
+                {
+                    services.map(service=><Service
+                    key={service.id}
+                    service={service}
+                    ></Service>)
+                }
+            </div>
+            
         </div>
     );
 };
